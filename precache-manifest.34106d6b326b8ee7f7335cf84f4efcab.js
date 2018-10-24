306a02df9b223451b55f91d6c92d451c2daefc12
Eric Zhou <eric.zhou0815@gmail.com>
2018-10-24 17:56:11 +1300

    Updates

---
+++ precache-manifest.34106d6b326b8ee7f7335cf84f4efcab.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf223f2176f375c08912191ba7592b3d",
+    "url": "/Ericada/static/media/QualitySouvenirs.cf223f21.png"
+  },
+  {
+    "revision": "d23c87843d6ec6bc72f8",
+    "url": "/Ericada/static/css/main.c4c7dd17.chunk.css"
+  },
+  {
+    "revision": "f3234f75b1f2ef7891bb",
+    "url": "/Ericada/static/js/1.f3234f75.chunk.js"
+  },
+  {
+    "revision": "e0f23168ecd3a029687e",
+    "url": "/Ericada/static/js/runtime~main.e0f23168.js"
+  },
+  {
+    "revision": "6c197de3b2179b5135b4c1f65a24b844",
+    "url": "/Ericada/static/media/me.6c197de3.png"
+  },
+  {
+    "revision": "7b8a5d34e3d0d0886db8997d2662b027",
+    "url": "/Ericada/static/media/Boad-Seat-Reservation.7b8a5d34.png"
+  },
+  {
+    "revision": "d23c87843d6ec6bc72f8",
+    "url": "/Ericada/static/js/main.d23c8784.chunk.js"
+  },
+  {
+    "revision": "a9cf5947c1b4ffcf857555f1c8a59cd1",
+    "url": "/Ericada/static/media/addnote.a9cf5947.PNG"
+  },
+  {
+    "revision": "e1af636076c7443513f821308b2d2866",
+    "url": "/Ericada/static/media/AddStrip.e1af6360.png"
+  },
+  {
+    "revision": "e996f54c165fc8c92e29058e5ca0fca5",
+    "url": "/Ericada/static/media/hero-portfolio.e996f54c.jpg"
+  },
+  {
+    "revision": "d62162d2d1a7d1e2417f98dc9bcb915f",
+    "url": "/Ericada/static/media/hero-contact.d62162d2.jpg"
+  },
+  {
+    "revision": "70764b84984982244ae3c0f385e9b9fb",
+    "url": "/Ericada/static/media/hero-image.70764b84.jpg"
+  },
+  {
+    "revision": "93b71b30ef37f24f49e146c98818ada0",
+    "url": "/Ericada/index.html"
+  }
+];
