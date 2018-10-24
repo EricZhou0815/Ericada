d1da2cf94e760a885e5d5fdc0905d9aa4a00e811
Eric Zhou <eric.zhou0815@gmail.com>
2018-10-24 17:47:41 +1300

    save

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,7 +2,8 @@ import React,{Component} from 'react';
 import ProjectCard from '../components/project-card/ProjectCard';
 import boatseatreservation from '../images/Boad-Seat-Reservation.png';
 import qualitysouvenirs from '../images/QualitySouvenirs.png';
-import QS from '../images/QS.png';
+import noteapp from '../images/addnote.PNG';
+import addstrip from '../images/AddStrip.png';
 
 class Portfolio extends Component {
     render() {
@@ -21,16 +22,24 @@ class Portfolio extends Component {
                 description:'E-commerce website with ASP.net Core Framework',
                 techStacks:['C#','ASP.NET Core MVC','jQuery','MSSQL'],
                 demoUrl:'https://dochyper.unitec.ac.nz/zhouz32/asp_application1',
-                codeUrl:'http://github.com/EricZhou0815/Boad-Seat-Reservation'
+                codeUrl:'https://github.com/EricZhou0815/QualitySouvenirs-ASP'
             },  
             {
-                imageUrl:QS,
-                name:'Quality Souvenirs (React & PHP)',
-                description:'E-commerce website with React and Laravel',
-                techStacks:['React','jQuery','Ajax','PHP','Laravel','MySQL'],
-                demoUrl:'http://ericzhou0815.github.io/Boad-Seat-Reservation/',
-                codeUrl:'https://github.com/EricZhou0815/QualitySouvenirs'
+                imageUrl:noteapp,
+                name:'NoteApp',
+                description:'A C# applicatin allow user to manage notes',
+                techStacks:['C#'],
+                demoUrl:'https://github.com/EricZhou0815/NoteApp',
+                codeUrl:'https://github.com/EricZhou0815/NoteApp'
             },  
+            {
+                imageUrl:addstrip,
+                name:'AddStrip',
+                description:'A C# mini claculator',
+                techStacks:['C#'],
+                demoUrl:'https://github.com/EricZhou0815/AddStrip',
+                codeUrl:'https://github.com/EricZhou0815/AddStrip'
+            }
         ];
 
       return (
