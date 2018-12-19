4dabd93adfab5ca3a025f4dd26c269a958968507
Eric Zhou <eric.zhou0815@gmail.com>
2018-12-19 22:20:50 +1300

    Updates

---
+++ precache-manifest.4da96c4e75a4e1403245ec48d15ddbd3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1af636076c7443513f821308b2d2866",
+    "url": "/Ericada/static/media/AddStrip.e1af6360.png"
+  },
+  {
+    "revision": "9fa59c337154f847e437",
+    "url": "/Ericada/static/css/main.765690b1.chunk.css"
+  },
+  {
+    "revision": "fb0e658e37ff55c3ca08",
+    "url": "/Ericada/static/js/1.fb0e658e.chunk.js"
+  },
+  {
+    "revision": "e0f23168ecd3a029687e",
+    "url": "/Ericada/static/js/runtime~main.e0f23168.js"
+  },
+  {
+    "revision": "6c197de3b2179b5135b4c1f65a24b844",
+    "url": "/Ericada/static/media/me.6c197de3.png"
+  },
+  {
+    "revision": "7b8a5d34e3d0d0886db8997d2662b027",
+    "url": "/Ericada/static/media/Boad-Seat-Reservation.7b8a5d34.png"
+  },
+  {
+    "revision": "cf223f2176f375c08912191ba7592b3d",
+    "url": "/Ericada/static/media/QualitySouvenirs.cf223f21.png"
+  },
+  {
+    "revision": "a9cf5947c1b4ffcf857555f1c8a59cd1",
+    "url": "/Ericada/static/media/addnote.a9cf5947.PNG"
+  },
+  {
+    "revision": "9fa59c337154f847e437",
+    "url": "/Ericada/static/js/main.9fa59c33.chunk.js"
+  },
+  {
+    "revision": "f29d769ec27a726d89604a28e29cba3e",
+    "url": "/Ericada/static/media/QS.f29d769e.png"
+  },
+  {
+    "revision": "1d56d8acdfd6b6fe1b3f26fa57e79145",
+    "url": "/Ericada/static/media/fraction.1d56d8ac.png"
+  },
+  {
+    "revision": "7ac278d63500bc43a6f3c9a80db973b0",
+    "url": "/Ericada/static/media/hyggee.7ac278d6.png"
+  },
+  {
+    "revision": "e996f54c165fc8c92e29058e5ca0fca5",
+    "url": "/Ericada/static/media/hero-portfolio.e996f54c.jpg"
+  },
+  {
+    "revision": "d62162d2d1a7d1e2417f98dc9bcb915f",
+    "url": "/Ericada/static/media/hero-contact.d62162d2.jpg"
+  },
+  {
+    "revision": "70764b84984982244ae3c0f385e9b9fb",
+    "url": "/Ericada/static/media/hero-image.70764b84.jpg"
+  },
+  {
+    "revision": "34a7bf1df07ddf4a1b44abc7087a9eab",
+    "url": "/Ericada/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ca571025aa74a68a0aa3b26605efd454.js"
+  "/Ericada/precache-manifest.4da96c4e75a4e1403245ec48d15ddbd3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Ericada/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
