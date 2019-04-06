a19457f1da7a4a43dc58e01bb3807a7e822cf56c
Eric Zhou <eric.zhou0815@gmail.com>
2019-04-06 10:54:35 +0800

    add likn security code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ const Footer=()=> {
       return (
         <footer className="page-footer font-small bg-dark myFooter">
                 <div className="mySocialMedia">
-                    <a href="https://github.com/EricZhou0815" target="_blank"><i className="fab fa-github fa-2x"></i></a>
-                    <a href="https://www.linkedin.com/in/eric-zhou-62910628/"  target="_blank"><i className="fab fa-linkedin fa-2x"></i></a>
+                    <a href="https://github.com/EricZhou0815" target="_blank" rel="noopener noreferrer"><i className="fab fa-github fa-2x"></i></a>
+                    <a href="https://www.linkedin.com/in/eric-zhou-62910628/"  target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin fa-2x"></i></a>
                 </div>
             <div className="footer-copyright text-center py-3 text-light">© 2018 Copyright: Eric Zhou. Developed with ReactJS.</div>
         </footer>
